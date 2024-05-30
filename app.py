94b86afe943093b964429ae563934b50afa992b4
Bindu Hegde <binduhegde1932@gmail.com>
2024-05-30 10:01:29 +0530

    add css to input box

--- app.py
+++ app.py
@@ -11,14 +11,67 @@ def main():
 
     st.write("Enter the Sudoku puzzle below. Use 0 for empty cells.")
     
+    # Custom CSS to style the input grid
+    st.markdown("""
+        <style>
+        .input-container {
+            display: grid;
+            grid-template-columns: repeat(9, 1fr);
+            gap: 2px;
+            width: 180px;
+            margin: 0 auto;
+        }
+        .input-container div {
+            position: relative;
+            width: 20px;
+            height: 20px;
+        }
+        .input-container div:before {
+            content: '';
+            display: block;
+            padding-top: 100%;
+        }
+        .input-container input {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            box-sizing: border-box;
+            text-align: center;
+            border: 1px solid #ccc;
+        }
+        .input-container div:nth-child(3n+1) {
+            border-left: 2px solid black;
+        }
+        .input-container div:nth-child(-n+3),
+        .input-container div:nth-child(n+7):nth-child(-n+9) {
+            border-top: 2px solid black;
+        }
+        .input-container div:nth-child(9n+1) {
+            border-left: 2px solid black;
+        }
+        .input-container div:nth-child(9n-7) {
+            border-right: 2px solid black;
+        }
+        .input-container div:nth-child(n+55) {
+            border-bottom: 2px solid black;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
     # Create a 9x9 grid for user input
     board = np.zeros((9, 9), dtype=int)
-    for i in range(9):
-        cols = st.columns(9)
-        for j in range(9):
-            board[i, j] = cols[j].number_input("", min_value=0, max_value=9, step=1, key=f"{i}-{j}")
+    with st.form(key='sudoku_form'):
+        input_container = st.empty()
+        input_container.markdown('<div class="input-container">', unsafe_allow_html=True)
+        for i in range(9):
+            for j in range(9):
+                board[i, j] = st.number_input("", min_value=0, max_value=9, step=1, key=f"{i}-{j}", format='%d')
+        input_container.markdown('</div>', unsafe_allow_html=True)
+        submitted = st.form_submit_button("Solve")
 
-    if st.button("Solve"):
+    if submitted:
         solved_board = solve_sudoku(board)
         st.write("Solved Sudoku Board:")
         st.write(solved_board)
